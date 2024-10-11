bc1b6d67367dd92d430cc1c8fbebe0c532fe90d6
Adrián Hervás <adrian.hervasmartinez@gmail.com>
2024-10-11 16:45:50 +0200

    updates font settings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -64,8 +64,47 @@ config.colors = {
 	},
 }
 
-config.font = wezterm.font("Monaspace Neon", { weight = "Regular" })
-config.font_size = 13
+config.font_size = 12
+config.warn_about_missing_glyphs = false
+config.freetype_load_target = "HorizontalLcd" -- https://wezfurlong.org/wezterm/config/lua/config/freetype_load_target.html
+
+-- Monaspace:  https://monaspace.githubnext.com/
+-- Based upon, contributed to:  https://gist.github.com/ErebusBat/9744f25f3735c1e0491f6ef7f3a9ddc3
+config.font = wezterm.font({ -- Normal text
+	family = "Monaspace Neon",
+	weight = "Regular",
+	harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
+})
+
+config.font_rules = {
+	{ -- Italic
+		intensity = "Normal",
+		italic = true,
+		font = wezterm.font({
+			family = "Monaspace Xenon", -- courier-like
+			style = "Italic",
+		}),
+	},
+
+	{ -- Bold
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font({
+			family = "Monaspace Krypton",
+			weight = "Bold",
+		}),
+	},
+
+	{ -- Bold Italic
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font({
+			family = "Monaspace Xenon",
+			style = "Italic",
+			weight = "Bold",
+		}),
+	},
+}
 
 config.enable_tab_bar = false
 
